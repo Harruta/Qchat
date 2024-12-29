9694cadca3982814cbcc912002e05597dfee1ce4
Haruta Tanaka <haruubusinesss@gmail.com>
2024-12-29 12:18:18 +0530

    skibidi

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { login, logout, signup, updateProfile} from "../controllers/auth.controllers.js"
-import { protectedRoute } from "../middleware/auth.middleware.js";
+import { checkAuth, login, logout, signup, updateProfile} from "../controllers/auth.controllers.js"
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup",signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.put("/update-profile", protectedRoute, updateProfile);
+router.put("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, checkAuth);
 
 export default router;
