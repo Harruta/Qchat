57af999c8aa12bffd8f5b3a180890d09e3154e82
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-01 23:33:38 +0530

    validation

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -9,6 +9,7 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from './store/useAuthStore.js';
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth }= useAuthStore()
@@ -35,6 +36,8 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage/>}/>
         <Route path="/profile" element={authUser ? <ProfilePage/> : <Navigate to="/login"/>}/>
       </Routes>
+
+      <Toaster />
     </div>
   )
 }
