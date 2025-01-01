4fc99a37b1a23f7867b7733fb45919545091d235
Haruta Tanaka <haruubusinesss@gmail.com>
2025-01-01 23:14:26 +0530

    bug fix

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -4,16 +4,16 @@ import { axiosInstance } from "../lib/axios";
 export const useAuthStore = create((set) => ({
     authUser: null,
     isSigningUp: false,
-    isLoading: false, // Corrected typo
+    isLoading: false, 
     isUpdatingProfile: false,
     isCheckingAuth: true,
 
     checkAuth: async () => {
         try {
-            const res = await axiosInstance.get("/auth/check");
+            const res = await axiosInstance.get("/auth/signup", data);
             set({ authUser: res.data });
         } catch (error) {
-            console.error("Error in checkAuth: ", error); // Updated to `console.error` for better logging
+            console.error("Error in checkAuth: ", error); 
             set({ authUser: null });
         } finally {
             set({ isCheckingAuth: false });
